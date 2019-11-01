64d661921c3130fd653988387d5209a562a0f96a
Fabián Montealegre <fabimc@gmail.com>
2019-10-31 23:51:40 -0600

    Added custom ftp bol provider

--- api/image/models/Image.js
+++ api/image/models/Image.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Lifecycle callbacks for the `Carousel` model.
+ * Lifecycle callbacks for the `Image` model.
  */
 
 module.exports = {
---
+++ providers/strapi-provider-upload-ftp-bol/lib/index.js
@@ -0,0 +1,111 @@
+/**
+ * Module dependencies
+ */
+const FTP = require("ftp");
+
+module.exports = {
+  provider: "FTP BOL",
+  name: "FTP BOL",
+  auth: {
+    host: {
+      label: "Host",
+      type: "text",
+    },
+    port: {
+      label: "Port",
+      type: "text",
+    },
+    user: {
+      label: "User",
+      type: "text",
+    },
+    password: {
+      label: "Password",
+      type: "password",
+    },
+    baseUrl: {
+      label: "Base URL",
+      type: "text",
+    },
+    // secure: {
+    //   label: "Secure",
+    //   type: "checkbox",
+    //   required: false,
+    // },
+  },
+  init: (config) => {
+    const { host, port, user, password, baseUrl } = config;
+
+    const ftp = new FTP();
+
+    ftp.connect({
+      host,
+      port,
+      user,
+      password,
+    });
+
+    const connection = new Promise((resolve, reject) => {
+      ftp.on("ready", () => {
+        resolve();
+      });
+
+      ftp.on("error", (err) => {
+        reject(err);
+      });
+    });
+
+    return {
+      upload(file) {
+        return new Promise((resolve, reject) => {
+          connection.then(() => {
+            ftp.list((err, list) => {
+              if (err) {
+                return reject(err);
+              }
+
+              const originalFileName = file.name.split(".")[0]; // removing the extension from the name
+
+              let fileName = `${originalFileName}${file.ext}`;
+              let c = 0;
+
+              const compareNames = file => file.name === fileName;
+
+              while (list.some(compareNames)) {
+                c += 1;
+                fileName = `${originalFileName}(${c})${file.ext}`;
+              }
+
+              ftp.append(file.buffer, `/www/img/mkt/${fileName}`, (err) => {
+                if (err) {
+                  return reject(err);
+                }
+
+                file.public_id = fileName;
+                file.url = baseUrl + fileName;
+                ftp.end();
+
+                return resolve();
+              });
+            });
+          });
+        });
+      },
+      delete(file) {
+        return new Promise((resolve, reject) => {
+          connection.then(() => {
+            ftp.delete(file.public_id, (err) => {
+              if (err) {
+                return reject(err);
+              }
+
+              ftp.end();
+
+              return resolve();
+            });
+          });
+        });
+      },
+    };
+  },
+};
