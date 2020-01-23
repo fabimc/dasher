5655a7bc0468b4d34755f4b4856a2f3883ec1206
Fabián Montealegre <fabimc@gmail.com>
2020-01-22 22:30:32 -0600

    Extended rotator controller

--- api/rotator/controllers/Rotator.js
+++ api/rotator/controllers/Rotator.js
@@ -1,8 +1,26 @@
 'use strict';
-
+const { sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/guides/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.rotator.search(ctx.query);
+    } else {
+      entities = await strapi.services.rotator.find(ctx.query, ['slides.desktop_login.file', 'slides.desktop_logout.file', 'slides.mobile_login.file', 'slides.mobile_logout.file']);
+    }
+
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models.rotator })
+    );
+  },
+
+  async findOne(ctx) {
+    const entity = await strapi.services.rotator.findOne(ctx.params, ['slides.desktop_login.file', 'slides.desktop_logout.file', 'slides.mobile_login.file', 'slides.mobile_logout.file']);
+    return sanitizeEntity(entity, { model: strapi.models.rotator });
+  },
+};
