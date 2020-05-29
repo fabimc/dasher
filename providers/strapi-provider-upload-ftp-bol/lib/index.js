d116a3acbb6f0dee63e6b5063db62ec47847735d
Fabián Montealegre <fabimc@gmail.com>
2020-05-28 21:46:58 -0600

    Added image optimization

--- providers/strapi-provider-upload-ftp-bol/lib/index.js
+++ providers/strapi-provider-upload-ftp-bol/lib/index.js
@@ -1,35 +1,38 @@
 /**
  * Module dependencies
  */
-const FTP = require("ftp");
+const FTP = require('ftp')
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
 
 module.exports = {
-  provider: "FTP BOL",
-  name: "FTP BOL",
+  provider: 'FTP BOL',
+  name: 'FTP BOL',
   auth: {
     host: {
-      label: "Host",
-      type: "text"
+      label: 'Host',
+      type: 'text'
     },
     port: {
-      label: "Port",
-      type: "text"
+      label: 'Port',
+      type: 'text'
     },
     user: {
-      label: "User",
-      type: "text"
+      label: 'User',
+      type: 'text'
     },
     password: {
-      label: "Password",
-      type: "password"
+      label: 'Password',
+      type: 'password'
     },
     baseUrl: {
-      label: "Base URL",
-      type: "text"
+      label: 'Base URL',
+      type: 'text'
     },
     basePath: {
-      label: "Base Path",
-      type: "text"
+      label: 'Base Path',
+      type: 'text'
     }
     // secure: {
     //   label: "Secure",
@@ -38,78 +41,105 @@ module.exports = {
     // },
   },
   init: config => {
-    const { host, port, user, password, baseUrl, basePath } = config;
+    const { host, port, user, password, baseUrl, basePath } = config
 
-    const ftp = new FTP();
+    const ftp = new FTP()
 
     ftp.connect({
       host,
       port,
       user,
       password
-    });
+    })
 
     const connection = new Promise((resolve, reject) => {
-      ftp.on("ready", () => {
-        resolve();
-      });
+      ftp.on('ready', () => {
+        resolve()
+      })
 
-      ftp.on("error", err => {
-        reject(err);
-      });
-    });
+      ftp.on('error', err => {
+        reject(err)
+      })
+    })
 
     return {
-      upload(file) {
+      upload (file) {
         return new Promise((resolve, reject) => {
           connection.then(() => {
             ftp.list(basePath, (err, list) => {
               if (err) {
-                return reject(err); 
+                return reject(err)
               }
 
-              const originalFileName = file.name.split(".")[0]; // removing the extension from the name
+              const originalFileName = file.name.split('.')[0] // removing the extension from the name
 
-              let fileName = `${originalFileName}${file.ext}`;
-              let c = 0;
-              
-              const compareNames = file => file.name === fileName;
+              let fileName = `${originalFileName}${file.ext}`
+              let c = 0
+
+              const compareNames = file => file.name === fileName
 
               while (list.some(compareNames)) {
-                c += 1;
-                fileName = `${originalFileName}(${c})${file.ext}`;
+                c += 1
+                fileName = `${originalFileName}(${c})${file.ext}`
               }
 
-              ftp.append(file.buffer, `${basePath}${fileName}`, err => {
-                if (err) {
-                  return reject(err);
-                }
+              if (['.jpg', '.png'].includes(file.ext)) {
+                return imagemin
+                  .buffer(file.buffer, {
+                    plugins: [
+                      imageminMozjpeg({ quality: 50 }),
+                      imageminPngquant({
+                        quality: [0.6, 0.8]
+                      })
+                    ]
+                  })
+                  .then(outBuffer => {
+                    ftp.append(outBuffer, `${basePath}${fileName}`, err => {
+                      if (err) {
+                        return reject(err)
+                      }
 
-                file.public_id = fileName;
-                file.url = baseUrl + fileName;
-                ftp.end();
+                      file.public_id = fileName
+                      file.url = baseUrl + fileName
 
-                return resolve();
-              });
-            });
-          });
-        });
+                      ftp.end()
+
+                      return resolve()
+                    })
+                  })
+              } else {
+                ftp.append(file.buffer, `${basePath}${fileName}`, err => {
+                  if (err) {
+                    return reject(err)
+                  }
+
+                  file.public_id = fileName
+                  file.url = baseUrl + fileName
+
+                  ftp.end()
+
+                  return resolve()
+                })
+              }
+            })
+          })
+        })
       },
-      delete(file) {
+      delete (file) {
         return new Promise((resolve, reject) => {
           connection.then(() => {
             ftp.delete(file.public_id, err => {
               if (err) {
-                return reject(err);
+                return reject(err)
               }
 
-              ftp.end();
+              ftp.end()
 
-              return resolve();
-            });
-          });
-        });
+              return resolve()
+            })
+          })
+        })
       }
-    };
+    }
   }
-};
+}
